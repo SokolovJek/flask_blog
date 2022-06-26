10b67e3a2d8cee9d90b8cf7e40495dd1cb6d37aa
Соколов Евгений <81033310+SokolovJek@users.noreply.github.com>
2022-06-26 21:43:12 +0300

    Delete config.py

--- flask_blog/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    # ссылка на путь к БД
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    # параметр неоюхадим для использования подписи в сеансовых cookie-файлов с целью зажиты их от поделок
-    SECRET_KEY = '2347sdf2k3u09123423k4h1239882jkjhs'
