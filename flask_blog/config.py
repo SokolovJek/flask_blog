2a855a23e0a2005b62a781485d6fbacf22d40983
Соколов Евгений <81033310+SokolovJek@users.noreply.github.com>
2022-07-14 15:40:23 +0300

    Update config.py

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -2,10 +2,10 @@ class Config:
     # ссылка на путь к БД
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     # параметр неоюхадим для использования подписи в сеансовых cookie-файлов с целью зажиты их от поделок
-    SECRET_KEY = '2347sdf2k3u091***************23423k4h1239882jkjhs'
+    SECRET_KEY = '2347sdf2k3u09123423k4h1239882jkjhs'
     MAIL_SERVER = 'smtp.yandex.ru'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = '******@yandex.ru'
-    MAIL_PASSWORD = '****пароль*****'
+    MAIL_USERNAME = 'sokolov199121@yandex.ru'
+    MAIL_PASSWORD = '7daragn7lora'
